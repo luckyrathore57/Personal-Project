74861617167aeadd7d43bb6f4c25b92cd3c2cd93
Lucky <es1210189@iitd.ac.in>
2024-01-30 02:31:39 +0530

    Add untracked files

---
+++ chess_project/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import ChessBoard from './component/chessBoard/ChessBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <ChessBoard></ChessBoard>
+    </div>
+  );
+}
+
+export default App;
